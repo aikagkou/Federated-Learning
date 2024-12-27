b9e91b140d76b1057236e7ef312f6e094f1d6a0b
aikagkou <84076707+aikagkou@users.noreply.github.com>
2024-12-27 12:43:04 +0200

    Update federated.py

--- ml/federated.py
+++ ml/federated.py
@@ -1,1 +1,31 @@
+import random
+from typing import Tuple
+import copy
+import numpy as np
+import torch
+import torch.nn.functional as F
+from config import federated_args, str2bool
+from ml.utils.train_utils import train, test
+
+# Load arguments
+args = federated_args()
+print(f"Script arguments: {args}\n")
+
+# Enable Cuda if available
+if torch.cuda.is_available():
+    device = args.device
+else:
+    device = 'cpu'
+
+# ensure reproducibility
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+# Load dataset
+from dataset.load_dataset import load_dataset()
+trainset, testset = load_dataset()
 
